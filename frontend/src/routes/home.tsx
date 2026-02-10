89831909107f9916e05c38d0dbba8bd8ef84f26d
rikardbq <j10rikardb@gmail.com>
2026-02-10 04:10:26 +0100

    add tailwind + daisyUI

--- frontend/src/routes/home.tsx
+++ frontend/src/routes/home.tsx
@@ -2,11 +2,11 @@ import { Link } from "react-router";
 
 import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
-import "../App.css";
+import "../app.css";
 
 export default () => {
     return (
-        <>
+        <div>
             <div>
                 <a href="https://vite.dev" target="_blank">
                     <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -19,8 +19,14 @@ export default () => {
                     />
                 </a>
             </div>
+            <button className="btn btn-success" onClick={() => {
+                document.getElementsByTagName("body")[0].dataset.theme = "light";
+            }}>Button</button>
+            <button className="btn btn-secondary" onClick={() => {
+                document.getElementsByTagName("body")[0].dataset.theme = "mys";
+            }}>Two</button>
             <h1>Home</h1>
             <Link to="/about">About</Link>
-        </>
+        </div>
     );
 };
