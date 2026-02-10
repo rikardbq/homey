973f38c15eb6edd976b2cbb1a60d7a9cf8b89c77
rikardbq <j10rikardb@gmail.com>
2026-02-10 04:31:02 +0100

    add more daisyui stuff

--- frontend/src/routes/home.tsx
+++ frontend/src/routes/home.tsx
@@ -19,14 +19,73 @@ export default () => {
                     />
                 </a>
             </div>
-            <button className="btn btn-success" onClick={() => {
-                document.getElementsByTagName("body")[0].dataset.theme = "light";
-            }}>Button</button>
-            <button className="btn btn-secondary" onClick={() => {
-                document.getElementsByTagName("body")[0].dataset.theme = "mys";
-            }}>Two</button>
+            <button
+                className="theme-controller btn btn-success"
+                value="vampire"
+            >
+                Button
+            </button>
+            <button className="theme-controller btn btn-secondary" value="mys">
+                Two
+            </button>
             <h1>Home</h1>
             <Link to="/about">About</Link>
+
+            <div className="dropdown mb-72">
+                <div tabIndex={0} role="button" className="btn m-1">
+                    Theme
+                    <svg
+                        width="12px"
+                        height="12px"
+                        className="inline-block h-2 w-2 fill-current opacity-60"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 2048 2048"
+                    >
+                        <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+                    </svg>
+                </div>
+                <ul
+                    tabIndex={-1}
+                    className="dropdown-content bg-base-300 rounded-box z-1 w-52 p-2 shadow-2xl"
+                >
+                    <li>
+                        <input
+                            type="radio"
+                            name="theme-dropdown"
+                            className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
+                            aria-label="Light"
+                            value="light"
+                        />
+                    </li>
+                    <li>
+                        <input
+                            type="radio"
+                            name="theme-dropdown"
+                            className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
+                            aria-label="Dracula"
+                            value="dracula"
+                        />
+                    </li>
+                    <li>
+                        <input
+                            type="radio"
+                            name="theme-dropdown"
+                            className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
+                            aria-label="Vampire"
+                            value="vampire"
+                        />
+                    </li>
+                    <li>
+                        <input
+                            type="radio"
+                            name="theme-dropdown"
+                            className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
+                            aria-label="Mys"
+                            value="mys"
+                        />
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };
