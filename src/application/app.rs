0b7fdb915df5ee313a694052b1c2271df2816b8e
rikardbq <j10rikardb@gmail.com>
2026-02-17 10:50:35 +0100

    add support for --no-context flag to disable context-menu in the app

--- src/application/app.rs
+++ src/application/app.rs
@@ -2,7 +2,8 @@ use winit::{
     application::ApplicationHandler,
     event::WindowEvent,
     event_loop::ActiveEventLoop,
-    window::{Window, WindowAttributes, WindowId},
+    platform::windows::IconExtWindows,
+    window::{Icon, Window, WindowAttributes, WindowId},
 };
 use wry::{WebView, WebViewBuilder};
 
@@ -37,15 +38,19 @@ pub struct App {
     window: Option<Window>,
     webview: Option<WebView>,
     window_attributes: Option<WindowAttributes>,
+    initialization_script: Option<&'static str>,
     web_config: Option<WebConfig>,
 }
 
 impl App {
-    pub fn set_web_config(&mut self, web_config: WebConfig) {
-        self.web_config = Some(web_config);
-    }
     pub fn set_window_attributes(&mut self, attributes: WindowAttributes) {
         self.window_attributes = Some(attributes);
+    }
+    pub fn set_initialization_scrip(&mut self, script: &'static str) {
+        self.initialization_script = Some(script);
+    }
+    pub fn set_web_config(&mut self, web_config: WebConfig) {
+        self.web_config = Some(web_config);
     }
 }
 
@@ -59,17 +64,24 @@ impl ApplicationHandler for App {
             .window_attributes
             .to_owned()
             .or(Some(Window::default_attributes()))
-            .unwrap();
+            .unwrap()
+            .with_window_icon(Some(
+                Icon::from_path(&format!("{ROOT_DIR}/testicon.ico"), None).unwrap(),
+            ));
         let window = event_loop.create_window(win_attr).unwrap();
         let ipc_handler = IPCHandler::new(window.id());
-        let webview = WebViewBuilder::new()
+        let mut webview_builder = WebViewBuilder::new()
             .with_ipc_handler(ipc_handler.handlers)
             .with_url(format!(
                 "http://{}:{}",
                 web_config.hostname, web_config.port
-            ))
-            .build(&window)
-            .unwrap();
+            ));
+
+        if let Some(script) = self.initialization_script {
+            webview_builder = webview_builder.with_initialization_script(script);
+        }
+
+        let webview = webview_builder.build(&window).unwrap();
 
         self.window = Some(window);
         self.webview = Some(webview);
